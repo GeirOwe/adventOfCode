ae7a74dd87765a1fba809adfe2bbc53a3b566b1d
GeirOwe <geir.owe@hotmail.com>
2020-12-04 23:33:56 +0100

    the part 2 of day 4
    in progress ....

---
+++ day4p2.py
@@ -0,0 +1,72 @@
+# day 4 advent of code 2020
+# by GeirOwe
+# the challenge: https://adventofcode.com/2020/day/4
+
+import pandas as pd 
+
+#process the input file and return a dataframe with all passports
+def process_passports(pass_file):
+    #process input - move them into a dictionary to make it possible to process
+    onePassport = {}
+    listofPassports = []
+    for onePass in pass_file:
+        onePass = onePass.strip()
+        if onePass == '':
+            listofPassports.append(onePassport)
+            onePassport = {}
+        else:
+            # Converting string from the input file into dictionary
+            Dict = dict((x.strip(), y.strip()) 
+                for x, y in (element.split(':')  
+                for element in onePass.split(' ')))
+            onePassport.update(Dict)
+    # add the last row also - for some reason not included in loop
+    listofPassports.append(onePassport)
+    return listofPassports 
+#end of process_passports function
+
+#find passports with invalid data fields
+def check_all_fields(df):
+    #errPass = df[(df['byr'] >= 1920) & (df['byr'] <= 2002)] #byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    errPass = df
+    return errPass
+#end of check_all_fields function
+
+#find passports with errors - i.e. missing information
+def check_pass_for_err(df):
+    # any passport where these columns is missing info, null / NaN, are invalid
+    wrongPass = df[df['ecl'].isnull() | df['pid'].isnull() | df['eyr'].isnull() | df['hcl'].isnull() | df['byr'].isnull() | df['iyr'].isnull() | df['hgt'].isnull()]
+    
+    #the part two challenge
+    moreErrPass = check_all_fields(wrongPass)
+
+    invalidPassports = len(moreErrPass)
+    return invalidPassports
+#end check_pass_for_err function
+
+#challenge:
+# Count the number of valid passports - those that have all required fields. 
+# Treat cid as optional. In your batch file, how many passports are valid?
+validPassports = 0
+
+#read the test puzzle input
+pass_file = open('day4_test_puzzle_input.txt', 'r')
+#read the puzzle input
+#pass_file = open('day4_puzzle_input.txt', 'r')
+
+#process the input file - a dictionary is returned
+listofPassports = process_passports(pass_file)
+df = pd.DataFrame(listofPassports)      #make list all passports into a dataframe to check content
+passportsProcessed = len(df)            # number of passports processed
+
+#find passports with errors - i.e. missing information
+errPassCount = check_pass_for_err(df)
+validPassports = passportsProcessed - errPassCount
+
+#print out the results
+print()
+print('number of valid passports: ', validPassports)
+print('------------------------------------')
+print('number of passports in total: ', passportsProcessed)
+print('number of invalid passports: ', errPassCount)
+print()
