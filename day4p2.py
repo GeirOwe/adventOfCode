713de15fd43bb60e813620bc5b2c94afe6ad3b1c
GeirOwe <geir.owe@hotmail.com>
2020-12-07 18:31:40 +0100

    day4 part two challenge
    first attempt on day4 part 2

--- day4p2.py
+++ day4p2.py
@@ -26,23 +26,60 @@ def process_passports(pass_file):
 #end of process_passports function
 
 #find passports with invalid data fields
-def check_all_fields(df):
-    #errPass = df[(df['byr'] >= 1920) & (df['byr'] <= 2002)] #byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    errPass = df
-    return errPass
+def check_data_for_quality(df):
+    #check data quality in birth year - byr. 
+    # first convert from string to int
+    df['byr'] = df['byr'].astype(int) 
+    df = df[(df['byr'] >= 1920) & (df['byr'] <= 2002)] 
+    
+    #check data quality in issue year - iyr. 
+    # first convert from string to int
+    df['iyr'] = df['iyr'].astype(int) 
+    df = df[(df['iyr'] >= 2010) & (df['iyr'] <= 2020)] 
+    
+    # check data quality in expiration year - eyr. 
+    # first convert from string to int
+    df['eyr'] = df['eyr'].astype(int) 
+    df = df[(df['eyr'] >= 2020) & (df['eyr'] <= 2030)] 
+    
+    # split height in two columns
+    # one for height and one for wht measure is used; in or cm
+    # then check data quality in height - hgt.  
+    # mal:    s.str.extract(r'([ab])(\d)')
+    df['hgtn']=df.hgt.str.extract(r'(\d+)', expand = True)                           # extract numeric height from hgt
+    df['hgtm']=df.hgt.str.extract('([a-zA-Z ]+)', expand = False)                   # extract if this is in or cm
+    df['hgtn'] = df['hgtn'].astype(int)                                             # set new height column as int
+    df = df[((df['hgtn'] >= 150) & (df['hgtn'] <= 193) & (df['hgtm'] == 'cm')) |    # If cm, the number must be at least 150 and at most 193.
+    ( (df['hgtn'] >= 59) & (df['hgtn'] <= 76) & (df['hgtm'] == 'in') )]             # If in, the number must be at least 59 and at most 76.
+    
+    #hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    #patternX = '^#\w{6}$'
+    df = df[df['hcl'].str.count(r'(^#\w{6}$)') == 1]
+
+    #ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    df = df[df['ecl'].str.count(r'(^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$)') == 1]
+
+    #pid (Passport ID) - a nine-digit number, including leading zeroes.
+    df = df[df['pid'].str.count(r'(^[0-9]{9}$)') == 1]
+
+    return df
 #end of check_all_fields function
 
 #find passports with errors - i.e. missing information
-def check_pass_for_err(df):
-    # any passport where these columns is missing info, null / NaN, are invalid
-    wrongPass = df[df['ecl'].isnull() | df['pid'].isnull() | df['eyr'].isnull() | df['hcl'].isnull() | df['byr'].isnull() | df['iyr'].isnull() | df['hgt'].isnull()]
-    
-    #the part two challenge
-    moreErrPass = check_all_fields(wrongPass)
+def check_pass_for_missing_data(df):
+    # list all vald passports in a dataframe
+    correctPassPorts = df[df['ecl'].notnull() & df['pid'].notnull() & df['eyr'].notnull() & df['hcl'].notnull() & df['byr'].notnull() & df['iyr'].notnull() & df['hgt'].notnull()]
+    #print(correctPassPorts)
 
-    invalidPassports = len(moreErrPass)
-    return invalidPassports
-#end check_pass_for_err function
+    #the part two challenge - check that all data are within the rules for the fields
+    correctPassPorts = check_data_for_quality(correctPassPorts)
+    #print('after data quality check')
+    #print(correctPassPorts)
+
+    #number of valid passports
+    validPassports = len(correctPassPorts)
+    return validPassports
+#end check_pass_for_missing_data function
 
 #challenge:
 # Count the number of valid passports - those that have all required fields. 
@@ -50,9 +87,9 @@ def check_pass_for_err(df):
 validPassports = 0
 
 #read the test puzzle input
-pass_file = open('day4_test_puzzle_input.txt', 'r')
+#pass_file = open('day4_test_puzzle_input.txt', 'r')
 #read the puzzle input
-#pass_file = open('day4_puzzle_input.txt', 'r')
+pass_file = open('day4_puzzle_input.txt', 'r')
 
 #process the input file - a dictionary is returned
 listofPassports = process_passports(pass_file)
@@ -60,13 +97,13 @@ df = pd.DataFrame(listofPassports)      #make list all passports into a datafram
 passportsProcessed = len(df)            # number of passports processed
 
 #find passports with errors - i.e. missing information
-errPassCount = check_pass_for_err(df)
-validPassports = passportsProcessed - errPassCount
+validPassports = check_pass_for_missing_data(df)
+#validPassports = passportsProcessed - errPassCount
 
 #print out the results
 print()
 print('number of valid passports: ', validPassports)
 print('------------------------------------')
 print('number of passports in total: ', passportsProcessed)
-print('number of invalid passports: ', errPassCount)
+#print('number of invalid passports: ', errPassCount)
 print()
