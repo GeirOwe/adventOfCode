50698cc0a05c2326581159b153014eb7514934f4
GeirOwe <geir.owe@hotmail.com>
2020-12-11 16:10:38 +0100

    day8 part 2 solved
    finalized day8 part 2

--- day8p2.py
+++ day8p2.py
@@ -18,8 +18,6 @@ def check_if_about_to_loop(newPos, visitedPos, aboutToLoop):
     #if so, we are about to loop
     theSet = set(visitedPos) 
     if newPos in theSet: 
-        print()
-        print('------ about to loop -------')
         aboutToLoop = True
     else:
         visitedPos.append(newPos)       #store what position we are at; and validate aginst it later
@@ -52,6 +50,34 @@ def jump_around(theInstruction, currentPos, theAccumulator):
     return newPos, theAccumulator
 #end jump_around function
 
+#start function
+def swap_next_relevant_instr(allInstructions, startingPoint):
+    x = startingPoint
+    while x < len(allInstructions):
+        #keep the value that we swap - will be used if we have to swap back
+        beforeSwap = x
+        bsValue = allInstructions[x]
+        #split at ' ' - the right part cotnains the jump " 
+        # the left part contains the instruction
+        splitInstr = allInstructions[x].split(' ')
+        theAccContent = splitInstr[0].strip()
+        theJump = splitInstr[1].strip()
+        #check if this instruction is to be swapped - if not move to next instruction
+        if theAccContent == 'jmp':
+            allInstructions[x] = 'nop ' + theJump
+            x = len(allInstructions)
+        else:
+            if theAccContent == 'nop':
+                if (allInstructions[x] != 'nop +0'):        #jmp +0 is invalid
+                    allInstructions[x] = 'jmp ' + theJump
+                    x = len(allInstructions)
+                else:
+                    x = x +1
+            else:
+                x = x + 1      
+    return allInstructions, beforeSwap, bsValue
+#end function
+
 #read the test puzzle input
 #boot_file = open('day8_test_puzzle_input.txt', 'r')
 #read the puzzle input
@@ -62,25 +88,53 @@ boot_file = open('day8_puzzle_input2.txt', 'r')
 # Run your copy of the boot code. Immediately before any instruction 
 # is executed a second time, what value is in the accumulator?
 aboutToLoop = False
+atTheEnd = False
 theAccumulator = 0
 calcPos = []
+didISwap = False
 
 #get all instructions from boot file
 allInstructions = get_all_instructions(boot_file)
 
+#starting point is where to get back to if we loop
+startingPoint = 0
+beforeSwap = 0
+bsValue = ''
+
 newPos=0
 #execute instructions until loop is detected
-while aboutToLoop != True:
+while (aboutToLoop != True) & (atTheEnd != True):
     #check if we are about to loop
     calcPos, aboutToLoop = check_if_about_to_loop(newPos, calcPos, aboutToLoop)
     if aboutToLoop:
-        print('... what value is in the accumulator? ', theAccumulator)
-        print()
-        print('... where we have been: ')
-        print(calcPos)
-        print()
+        #reset and try again
+        #first - swap back if a swap has been done
+        if didISwap:
+            allInstructions[beforeSwap] = bsValue
+            startingPoint = startingPoint + 1
+        
+        #then - swap again in the next jmp / nop from where we are and forward
+        allInstructions, beforeSwap, bsValue = swap_next_relevant_instr(allInstructions, startingPoint)
+        startingPoint = beforeSwap 
+        
+        #reset the values and run all instructions again
+        aboutToLoop = False
+        theAccumulator = 0
+        calcPos = []
+        newPos=0
+        didISwap = True
     else:
         #jump to position as specified in instruction and collect accumulator value when relevant
         currentPos = newPos
+        #are we at the end of all instructions
         newPos, theAccumulator = jump_around(allInstructions[newPos], currentPos, theAccumulator)
+        if newPos == len(allInstructions):
+            print()
+            print('all instructions executed without a loop.')
+            print('... what value is in the accumulator? ', theAccumulator)
+            print()
+            print('... where we have been: ')
+            print(calcPos)
+            print()
+            atTheEnd = True
 
