244c09bb861e36b27108ada384df3d40bfed36e4
GeirOwe <geir.owe@hotmail.com>
2021-06-24 14:06:33 +0200

    Update day22019.py
    part2

--- day22019.py
+++ day22019.py
@@ -24,19 +24,32 @@ def process_codes(codeList):
     codeList[1] = 12
     codeList[2] = 2
 
-    #set iniial position
-    currentPos = 0
+    #set initial position
+    # The address of the current instruction is called the instruction pointer
+    # After an instruction finishes, the instruction pointer increases by the number of values in the instruction; 
+    # until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters)
+    currentPos = 0 
     cont = True
+    part2ExpectedResult = 19690720
     while cont:
+        #Opcodes (like 1, 2, or 99) mark the beginning of an instruction. 
         opCode = int(codeList[currentPos]) #find the opCode
         if opCode == 99:    #99 -> quit
             cont = False
         else:
+            # The values used immediately after an opcode, if any, are called the instruction's parameters.
             pos1 = int(codeList[currentPos+1])
             pos2 = int(codeList[currentPos+2])
             pos3 = int(codeList[currentPos+3])
             result = calc_result(int(codeList[pos1]), int(codeList[pos2]), opCode) #process the two numbers in pos1 and pos2
             codeList[pos3] = result #store the processed result in third position
+
+            #part2
+            part2_quiz = 100 * pos1 + pos2
+            if part2_quiz == part2ExpectedResult:
+                print(pos1, pos2)
+            
+            # After an instruction finishes, the instruction pointer increases by the number of values in the instruction
             currentPos += 4 #move forward to next opCode
 
     return codeList[0]
@@ -53,8 +66,15 @@ def get_the_data():
 
 #start function
 def start_the_challenge():
-    #get the data
+    #get the data and read the into  list
     codeList = get_the_data()
+
+    # Part 2 - With terminology out of the way, we're ready to proceed. To complete the gravity assist, 
+    # you need to determine what pair of inputs produces the output 19690720.
+    # Once the program has halted, its output is available at address 0, also just like before. Each time 
+    # you try a pair of inputs, make sure you first reset the computer's memory to the values in the program 
+    # (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+    # Find the input noun (pos2) and verb (pos3) that cause the program to produce the output 19690720
 
     #process the codes and return the answer
     valueAtPosZero = process_codes(codeList) 
