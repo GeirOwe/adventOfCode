6832aa7fa8f66ffb45671ab824a2e975e74915ea
GeirOwe <geir.owe@hotmail.com>
2021-12-03 15:51:12 +0100

    day3 part2 done
    day3 part2 done

--- day3_2_2021.py
+++ day3_2_2021.py
@@ -2,34 +2,59 @@
 # source: https://adventofcode.com/2021/day/3
 
 import os
-import numpy as np
+import pandas as pd
 
 def clear_console():
     os.system('clear')
     print('< .... AoC 2021 Day 3, part 2 .... >')
     print()
 
-def reduce_dataset(theData, theChar, currColumn):
-    df = pd.DataFrame(theData)
-    if theChar == 0:
-        #reduce data set to only keep rows with zeroes in current column
-        df = df.loc[df[currColumn] == 0]
-    else:
-        ##reduce data set to only keep rows with ones in current column
-        df = df.loc[df[currColumn] == 1]
-    theData = df.values.tolist()
-    return theData
+def reduce_dataset(theData, theSignal, currPos):
+    reduced_dataset = []
+    for row in theData:
+        if int(row[currPos]) == theSignal:
+            #reduce data set to only keep rows with zeroes in current column
+            reduced_dataset.append(row)
+    return reduced_dataset
+
+def process_co2(theData):
+    #set initial position for the dataset
+    noOfColumns = 5
+    #noOfColumns = 12
+    currColumn = 0
+    # loop thru the list and calculate changes to horizontal position and depth
+    while currColumn < noOfColumns:
+        zeroes = 0
+        ones = 0
+        #count 0 and 1 in current row
+        for row in theData:
+            if int(row[currColumn]) == 0:
+                zeroes += 1
+            else:
+                ones += 1
+        
+        #find least common bit
+        if zeroes <= ones:
+            #reduce data set to only keep rows with zeroes in current column
+            theData = reduce_dataset(theData, 0, currColumn)
+        else:
+            #reduce data set to only keep rows with ones in current column
+            theData = reduce_dataset(theData, 1, currColumn)
+        
+         # if only one row left -> done. keep that row as the oxy_gen_rate
+        if len(theData) == 1:
+            currColumn = noOfColumns
+        else:
+            currColumn += 1
+   
+    co2_rating = int(row.strip(), 2)
+    return co2_rating
 
 def process_oxy(theData):
     #set initial position for the dataset
     noOfColumns = 5
     #noOfColumns = 12
     currColumn = 0
-    valueX = 0
-    # Your horizontal position and depth both start at 0. so does aim.
-    gamma_rate = []
-    epsilon_rate = []
-    remaining_row = []
     # loop thru the list and calculate changes to horizontal position and depth
     while currColumn < noOfColumns:
         zeroes = 0
@@ -52,16 +77,15 @@ def process_oxy(theData):
          # if only one row left -> done. keep that row as the oxy_gen_rate
         if len(theData) == 1:
             currColumn = noOfColumns
-            remaining_row = theData
         else:
             currColumn += 1
    
-    oxy_gen_rate = int(remaining_row, 2)
+    oxy_gen_rate = int(row.strip(), 2)
     return oxy_gen_rate
 
 def process_the_data(theData):
     oxy_gen_rate = process_oxy(theData)
-    co2_rating = process_cow(theData)
+    co2_rating = process_co2(theData)
      
     #calculate final value 
     valueX = oxy_gen_rate * co2_rating
@@ -84,7 +108,7 @@ def start_the_engine():
     #get the data and read them into a list
     theData = get_the_data()
     
-    #process the data and return the answer -> correct answer is: 1856459736
+    #process the data and return the answer 
     valueX = process_the_data(theData) 
     
     print('What is the power consumption of the submarine -> ', valueX,'\n') 
---
+++ day3_3_2021.py
@@ -0,0 +1,23 @@
+get_vertical = lambda lst: [[y[x] for y in lst] for x in range(len(lst[0]))]
+get_value = lambda x, n, m: n if x.count('0') > x.count('1') else m
+calc_bin = lambda lst: int(''.join([str(x) for x in lst]), 2)
+
+with open("day32021_puzzle_input.txt", 'r') as file: 
+    data = file.read().splitlines()    
+    gamma = [get_value(x, 0, 1) for x in get_vertical(data)]
+    epsilon = [int(not(x)) for x in gamma]
+    print("part1: ", calc_bin(gamma) * calc_bin(epsilon)) 
+    oxy = data[:]
+    co2 = data[:]
+    for i,j in enumerate(data[0]):
+        if len(oxy) > 1:
+            current_oxy = get_value(get_vertical(oxy)[i], 0, 1)
+            oxy = [x for x in oxy if int(x[i]) == current_oxy]
+        if len(co2) > 1:
+            current_co2 = get_value(get_vertical(co2)[i], 1, 0)
+            co2 = [x for x in co2 if int(x[i]) == current_co2] 
+    print("part1: ", calc_bin(oxy) * calc_bin(co2))
+
+#let's start
+#if __name__ == '__main__':
+#    d03()
