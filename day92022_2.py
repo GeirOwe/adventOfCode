d32b85d29dc2e801f6e33e71ee67e6ad42d9451e
GeirOwe <geir.owe@hotmail.com>
2022-12-17 20:25:20 +0100

    day 9 part 2

--- day92022_2.py
+++ day92022_2.py
@@ -10,24 +10,77 @@ def clear_console():
     print()
     return
 
-def trail_size(theData):
-    trail = []
-    rad = [] 
-    # just preparing a grid / trail of 1000 x 1000
-    x = 30
-    y = 30
-    i = 0
-    j = 0
-    # prepare trail with dots
-    while j < y:
-        while i < x:
-            rad.append('.')
-            i += 1
-        trail.append(rad)
-        j += 1 
+#define a grid object
+class Grid():
+    def __init__(self, row_length, col_height):
+        self.row_length = row_length
+        self.col_height = col_height
+        self.grid_row = []
+        self.grid = []
+        self.trail_length = 0
 
-    trailX = np.array(trail)
-    return trailX
+    #the row length of the grid
+    def get_row_length(self):
+        return self.row_length
+
+    #the column height of the grid
+    def get_col_height(self):
+        return self.col_height
+
+    #add dots to grid
+    def initialize_grid(self):
+        i = 0
+        j = 0
+        # prepare trail with dots
+        while j < self.col_height:
+            while i < self.row_length:
+                self.grid_row.append('.')
+                i += 1
+            #row is complete
+            self.grid.append(self.grid_row)
+            #next row start in column zero
+            i = 0
+            #next row
+            j += 1 
+        self.grid = np.array(self.grid)
+
+    def mark_snake(self, row, col):
+        self.grid[row][col] = '#'
+    def get_trail_length(self):     #count where tail has been
+        self.trail_length = np.sum(self.grid == '#')
+        return self.trail_length
+#end class definition
+
+#define a snake object
+class Snake():
+    def __init__(self, tail_length):
+        self.tail_length = tail_length
+        self.head_pos_row = 0
+        self.head_pos_col = 0
+        self.prev_head_pos_row = 0
+        self.prev_head_pos_col = 0
+        self.tail_pos_row = 0
+        self.tail_pos_col = 0
+    
+    #the row length of the grid
+    def get_head_pos_row(self):
+        return self.head_pos_row
+    def get_head_pos_col(self):
+        return self.head_pos_col
+    def get_tail_pos_row(self):
+        return self.tail_pos_row
+    def get_tail_pos_col(self):
+        return self.tail_pos_col
+    def get_prev_head_pos_row(self):
+        return self.prev_head_pos_row
+    def get_prev_head_pos_col(self):
+        return self.prev_head_pos_col
+
+    #the column height of the grid
+    def get_tail_length(self):
+        return self.tail_length
+#end class definition
+
 
 def move_tail(hCol, hRow, tCol, tRow):
     # head moves only in straight lines - tail moves vertically when needed
@@ -76,56 +129,29 @@ def move_tail(hCol, hRow, tCol, tRow):
 # do the move and watch the tail
 # If the head is ever two steps directly up, down, left, or right from the tail, 
 # the tail must also move one step in that direction
-def do_the_move(trail, dir, move, hCol, hRow, tCol, tRow, tail_length):
+def do_the_move(snake, dir, move):
     i = 0
     # move head once and check distance to tail
     while i < move:
-        if dir == 'R': hCol += 1     #move right on same row
-        if dir == 'L': hCol -= 1     #move left on same row
-        if dir == 'U': hRow += 1     #move up a row
-        if dir == 'D': hRow -= 1     #move down a row
+        #move the head
+        snake.move_head(dir)
         # after each step, you'll need to update the position of the tail.
-        # track head_pos before it moves.
-        org_hCol = hCol
-        org_hRow = hRow
-        #move the complete tail
-        j = tail_length
+        snake.move_tail()
+        j = snake.get_tail_length
         while j > 0:
-            # track tail_pos before it moves.
-            pt_row = tRow
-            pt_col = tCol
-            hCol, hRow, tCol, tRow, tail_moved = move_tail(hCol, hRow, tCol, tRow)
-            #one less tail to check
-            j -= 1
-            #whenever head moves to a new spot - mark in the trail
-            if tail_moved: 
-                trail[tRow][tCol] = '#'
-
-                if tail_length < 9: tail_length += 1
-                # the tail becomes the new head, next tail position also needs to move
-                hCol = tCol
-                hRow = tRow
-                tCol = pt_col
-                tRow = pt_row
+            snake.move_tail()
         i += 1
-        #reset header data - since used to move tail
-        hRow = org_hRow
-        hCol = org_hCol
-
-    return trail, hCol, hRow, tCol, tRow, tail_length
+    return
 
 def process_the_data(theData):
-    #find answer
-    valueX = 0
-    #current pos
-    hCol = 0
-    hRow = 0
-    tCol = 0
-    tRow = 0
-    trail = trail_size(theData)
-    tail_length = 1
-    #start position
-    trail[0][0] = '#'
+    #create a snake object
+    snake = Snake(1)
+    #create a grid object
+    grid = Grid(5, 5)
+    grid.initialize_grid()
+    #grid.mark_snake(0, 0)
+    #print(snake.get_tail_pos_row(), snake.get_tail_pos_col())
+
     #do all the moves
     for row in theData:
         # fetch instruction
@@ -133,12 +159,9 @@ def process_the_data(theData):
         dir = splitCol[0]
         move = int(splitCol[1])
         #do the move
-        trail, hCol, hRow, tCol, tRow, tail_length  = do_the_move(trail, dir, move, hCol, hRow, tCol, tRow, tail_length)
-    
-    #count where tail has been
-    valueX = np.sum(trail == '#')
+        do_the_move(snake, grid, dir, move)
 
-    return valueX
+    return grid.get_trail_length()
 
 def get_the_data():
     #read the test puzzle input 
