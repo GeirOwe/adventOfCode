8099fdcf2ffb2049feb0e769072b6025473a19a7
GeirOwe <geir.owe@hotmail.com>
2023-12-17 21:20:49 +0100

    day10

--- day9_2_2023.py
+++ day9_2_2023.py
@@ -4,7 +4,7 @@ import os
 
 def clear_console():
     os.system('clear')
-    print('< .... AoC 2023 Day 9, part 1 .... >')
+    print('< .... AoC 2023 Day 9, part 2 .... >')
     print()
     return
 
@@ -23,18 +23,20 @@ def process_the_row(row_list):
     
     #check if all zeroes
     x=[i for i in next_row if i==0]
-    if len(x) == len(next_row): all_zeroes = True 
+    if len(x) == len(next_row): 
+        #print(next_row)
+        all_zeroes = True 
 
     #if not all zeroes repeat
     if all_zeroes != True:
         #recursion until zeroes
         next_row = process_the_row(next_row)
-        row_list.append(row_list[-1] + next_row[-1])
+        row_list.insert(0, (row_list[0] - next_row[0]))
         #print(row_list)
     else:
         #append to the row and repeat back thru all recursion
-        #print(next_row)
-        row_list.append(row_list[-1] + next_row[-1])
+        #insert in the eginning of the list
+        row_list.insert(0, (row_list[0] - next_row[0]))
         #print(row_list)
 
     return row_list
@@ -50,7 +52,7 @@ def process_the_data(theData):
             num_list.append(int(num))
         #process row
         num_list = process_the_row(num_list)
-        sumX += num_list[-1]
+        sumX += num_list[0]
         #empty num list before next row
         num_list = []
         #print()
